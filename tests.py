7508ecb64a766b3e2ede6260c2ddf1c44d9a7af5
varuntangudu <varuntangudu123@gmail.com>
2024-03-28 19:43:00 -0400

    Debugged Tests.py

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
 from server import app
+import json
 
 class TestWeatherEndpoints(unittest.TestCase):
 
@@ -9,21 +10,33 @@ class TestWeatherEndpoints(unittest.TestCase):
 
     def test_get_weather_records(self):
         # Test with valid query parameters
-        response = self.app.get('/api/weather?date=2024-03-27&station_id=123&page=1')
+        response = self.app.get('/api/weather?date=1985-01-01&station_id=USC00110072&page=0')
         self.assertEqual(response.status_code, 200)
+        data = json.loads(response.data)
+        if not data:
+            self.fail("NO data found with the given weather parameters")
 
         # Test with missing query parameters
         response = self.app.get('/api/weather')
         self.assertEqual(response.status_code, 200)
+        data = json.loads(response.data)
+        if not data:
+            self.assertEqual(response.data.decode(), "No valid Weather data")
 
     def test_get_weather_stats(self):
         # Test with valid query parameters
-        response = self.app.get('/api/weather/stats?date=2024-03-27&station_id=123&page=1')
+        response = self.app.get('/api/weather/stats?date=1985-01-01&station_id=USC00110072&page=0')
         self.assertEqual(response.status_code, 200)
+        data = json.loads(response.data)
+        if not data:
+            self.fail("NO data found with the given stats parameters")
 
         # Test with missing query parameters
         response = self.app.get('/api/weather/stats')
         self.assertEqual(response.status_code, 200)
+        data = json.loads(response.data)
+        if not data:
+            self.assertEqual(response.data.decode(), "No Valid Station Data")
 
 if __name__ == '__main__':
     unittest.main()
